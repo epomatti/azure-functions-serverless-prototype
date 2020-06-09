6311a6d03f24f65aed38fd5b8df51ff1f7be1f46
Evandro Pomatti <evandro.pomatti@gmail.com>
2020-06-09 18:06:28 -0300

    infrastructure

---
+++ infrastructure/main.tf
@@ -0,0 +1,24 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "maibeer"
+  location = "brazilsouth"
+}
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "cosmos-maibeer-prototype"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = false
+
+  consistency_policy {
+    consistency_level = "Sesssion"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+
+}
