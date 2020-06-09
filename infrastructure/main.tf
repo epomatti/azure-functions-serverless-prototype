ac5e00e742ddbed4c596ccf7f1146ee602d746b2
Evandro Pomatti <evandro.pomatti@gmail.com>
2020-06-09 18:41:24 -0300

    cosmos

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
   location = "brazilsouth"
 }
 
-resource "azurerm_cosmosdb_account" "db" {
+resource "azurerm_cosmosdb_account" "default" {
   name                = "cosmos-maibeer-prototype"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -27,6 +27,29 @@ resource "azurerm_cosmosdb_account" "db" {
 
 }
 
+resource "azurerm_cosmosdb_mongo_database" "default" {
+  name                = "maibeer"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.default.name
+  throughput          = 400
+}
+
+resource "azurerm_cosmosdb_sql_container" "questions" {
+  name                = "questions"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.default.name
+  database_name       = azurerm_cosmosdb_mongo_database.default.name
+  partition_key_path  = "product"
+}
+
+resource "azurerm_cosmosdb_sql_container" "answers" {
+  name                = "answers"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.default.name
+  database_name       = azurerm_cosmosdb_mongo_database.default.name
+  partition_key_path  = "address.zip"
+}
+
 output "cosmosdb_connection_strings" {
-  value = azurerm_cosmosdb_account.db.connection_strings
+  value = azurerm_cosmosdb_account.default.connection_strings
 }
