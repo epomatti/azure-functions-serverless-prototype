04452778b61a8ed52dc48c285a9c5ba792bb6c3f
Evandro Pomatti <evandro.pomatti@gmail.com>
2020-06-10 00:51:25 -0300

    key vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,18 @@
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
 }
+
+data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
   name     = "maibeer"
   location = "brazilsouth"
 }
+
 
 resource "azurerm_cosmosdb_account" "default" {
   name                = "cosmos-maibeer-prototype"
@@ -61,6 +68,37 @@ resource "azurerm_cosmosdb_mongo_collection" "answers" {
 
 }
 
+resource "azurerm_key_vault" "prototype" {
+  name                        = "kv-maibeer-prototype"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = false
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_enabled         = true
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  # access_policy {
+  #   tenant_id = data.azurerm_client_config.current.tenant_id
+  #   object_id = data.azurerm_client_config.current.object_id
+
+  #   key_permissions = [
+  #     "get",
+  #   ]
+  # }
+
+  network_acls {
+    default_action = "Allow"
+    bypass         = "AzureServices"
+  }
+
+}
+
 output "cosmosdb_connection_strings" {
   value = azurerm_cosmosdb_account.default.connection_strings
+}
+
+output "vault_uri" {
+  value = azurerm_key_vault.prototype.vault_uri
 }
